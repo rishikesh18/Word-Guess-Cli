2376602979c13d61249567056ac5511a72c3db38
rishikesh18 <rishi.neupane@gmail.com>
2018-05-13 19:21:46 -0400

    fixed underscores placeholders

--- Word.js
+++ Word.js
@@ -4,13 +4,25 @@ var Letter = require("./Letter");
 var Word = function(myWord) {	
 	this.myWord = myWord;	
 	this.letters = [];	
-	this.underscores = ["_"];
+	this.underscores = [""];
 	this.splitWord = function() {
-		this.letters = this.myWord.split("");		
-		numberUnderscoresNeeded = this.letters.length;		
-		console.log(this.underscores.join(" "));
+		this.letters = this.myWord.split("");	
+		numberUnderscoresNeeded = this.letters.length;
+		let newString=[];
+		for(let i=0;i<numberUnderscoresNeeded;i++){
+			newString[i] ="_";
+		}
+		for(let i=0 ;i<this.letters.length;i++){
+			for(let j=0;j<this.underscores.length;j++){
+				if(this.letters[i]==this.underscores[j]){
+					newString[i]=this.letters[i];
+				}
+			}
+		}
+		console.log(newString.join(" "));
 	}
 	this.generateLetters = function() {
+			
 		for (i=0; i < this.letters.length; i++){
 			this.letters[i] = new Letter (this.letters[i]);			
 			this.letters[i].showCharacter();
